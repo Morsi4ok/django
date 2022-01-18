59e724b3b4123e3db06bcec33ceeeeaeac4f95b0
Victor Kozlov <victorkozlov1999@mail.ru>
2022-01-18 19:53:20 +0300

    Add files

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from posts.models import Tags
+from posts.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "slug", "created_at")
+    fields = ("author", "title", "image", "slug", "text", "created_at")
+    readonly_fields = ("created_at",)
+    search_fields = ("title", "slug", "text")
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "posts")
+    fields = ("author", "title", "posts")
+    readonly_fields = ("posts",)
+    search_fields = ("title", "posts")
