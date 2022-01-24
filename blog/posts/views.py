48e0357b2bd80e110c13ff732b65fc4b6c3277aa
Victor Kozlov <victorkozlov1999@mail.ru>
2022-01-24 12:32:13 +0300

    add files

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,6 +5,9 @@ logger = logging.getLogger(__name__)
 
 
 def posts_index(request):
-    value = request.GET.get("key")
-    logger.info(value)
-    return HttpResponse("Posts index view")
+    result = ""
+    user = User.objects.get(username=request.GET.get("author","morsik"))
+    author_name = request.GET.get("author","morsik")
+    for x in Post.objects.filter(author__username=author_name).order_by("-id")
+
+    return HttpResponse(result)
